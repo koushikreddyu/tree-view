aad7747dc7c9dd0a5d53358610c457e8d47765ea
Koushik Reddy U <koushikreddyu@gmail.com>
2020-07-28 12:11:58 +0530

    Tree view initial commit

---
+++ src/components/TreeContainer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h3>Tree view</h3>
+        <Treeview :node="treeData" />
+    </div>
+</template>
+
+<script>
+import Treeview from './Treeview'
+export default {
+    name: "TreeContainer",
+    components: {
+        Treeview
+    },
+    data() {
+        return {
+            treeData : {
+                name: "Root",
+                children: [
+                    { name: "Child 1" },
+                    { name: "Child 2" },
+                    {
+                        name: "Child 3",
+                        children: [
+                        {
+                            name: "Sub Child 1",
+                            children: [{ name: "Inner Sub Child 1" }, { name: "Inner Sub Child 2" }]
+                        },
+                        { name: "Sub Child 2" },
+                        { name: "Sub Child 3" },
+                        {
+                            name: "Sub Child 4",
+                            children: [{ name: "Inner Sub Child 3" }, { name: "Inner Sub Child 4" }]
+                        }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
---
+++ src/components/Treeview.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div
+            @click="nodeClicked"
+            :style="{'margin-left': `${depth*20}px`}"
+        >{{node.name}}</div>
+        <div v-if="expanded">
+            <Treeview 
+                v-for="child in node.children"
+                :key="child.name"
+                :node="child"
+                :depth="depth+1"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Treeview",
+    props: {
+        node: Object,
+        depth: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            expanded: false
+        }
+    },
+    methods: {
+        nodeClicked() {
+            this.expanded = !this.expanded
+        }
+    }
+}
+</script>
